6d8a5d352762cf8441bdc210d5b3603d7d1f377b
Líllian Leite <lillianleitewd@gmail.com>
2023-09-07 23:06:23 -0300

    Add metodo contar

--- src/ControleDeFluxoDesafioDio.java
+++ src/ControleDeFluxoDesafioDio.java
@@ -10,11 +10,23 @@ public class ControleDeFluxoDesafioDio {
         int numero1 = scanner.nextInt();
         System.out.println("Digite outro número: ");
         int numero2 = scanner.nextInt();
-
         }
         catch(InputMismatchException e) {
             System.out.println("Os campos precisam ser numéricos.");
         }
         
+    }
+
+    static void contar(int numero1, int numero2) throws NumerosInvalidosExeption{
+        int contagem;
+
+        if (numero1 < numero2)
+            int contagem = numero2 - numero1;
+        else
+            System.out.println("O segundo número deve ser maior que o primeiro");
+        
+
+        for(contagem = 0; ;);
+
     }
 }
