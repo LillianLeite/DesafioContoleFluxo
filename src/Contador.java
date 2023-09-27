49053b8fb408b3a5a78e5ba39ef836fd69e4451c
Líllian Leite <lillianleitewd@gmail.com>
2023-09-27 17:28:58 -0300

    Atualização no for

--- src/Contador.java
+++ src/Contador.java
@@ -16,26 +16,25 @@ public class Contador {
         System.out.println("Houveram a seguintes iterações");
         contar(contagem);
 
-
         scanner.close();
 
-        if (numero1 > numero2){
+        while (numero1 > numero2) {
             throw new Exception("O segundo número deve ser maior que o primeiro");
-        }
-                }
+        }} 
 
         catch(InputMismatchException e){
             System.out.println("Os campos precisam ser numéricos");
         }
-        }
+            }
       catch(Exception ex) {
             System.out.println(ex.getMessage());
         }
-    }
-
+        }
+    
     static void contar(int contagem) {
         for(int acumular = 0; acumular < contagem; acumular ++){
-            System.out.println("Imprimindo número " + acumular);
+            System.out.println("Imprimindo número " + acumular );
         }
+    
     }
 }
